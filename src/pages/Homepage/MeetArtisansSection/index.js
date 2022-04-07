7a0d74fd3de93f9f7b5dbf1d816839dd5b584dba
whitegod <kmbagwu12@gmail.com>
2022-04-07 20:33:04 +0100

    working on slider on meet artisans section

--- src/pages/Homepage/MeetArtisansSection/index.js
+++ src/pages/Homepage/MeetArtisansSection/index.js
@@ -13,12 +13,20 @@ function MeetArtisansSection() {
   return (
     <section className="meet-artisans__section">
       <Container>
-        <h2>
+        <h2 className="meet-artisans__caption">
           Meet top <span className="accent">artisans</span>{' '}
         </h2>
       </Container>
 
-      <div className="meet-artisans__slider"></div>
+      <div className="meet-artisans__slider">
+        <div className="meet-artisans__slider-img">IMG 1</div>
+        <div className="meet-artisans__slider-img">IMG 2</div>
+        <div className="meet-artisans__slider-img">IMG 3</div>
+        <div className="meet-artisans__slider-img">IMG 4</div>
+
+        <button className="meet-artisans__btn meet-artisans__prev">prev</button>
+        <button className="meet-artisans__btn meet-artisans__next">Next</button>
+      </div>
     </section>
   );
 }
