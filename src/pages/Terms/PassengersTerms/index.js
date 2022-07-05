c2c77f5a49bb2e10e46943778ad5186f8deb6676
iamwhitegod <kmbagwu12@gmail.com>
2022-07-05 10:07:16 +0100

    added passenger terms

--- src/pages/Terms/PassengersTerms/index.js
+++ src/pages/Terms/PassengersTerms/index.js
@@ -1,7 +1,256 @@
 import React from 'react';
 
 function PassengersTerms() {
-  return <div>PassengersTerms</div>;
+  return (
+    <div className="terms">
+      <div className="container">
+        <div className="terms__passenger">
+          <h2>Passenger Terms and Conditions</h2>
+
+          <p>
+            These terms and conditions describe how to use the Bube app. Bube links
+            passengers and drivers to make city travel simpler.
+          </p>
+
+          <p>
+            Bube , a private limited company incorporated and registered under the laws
+            of Republic of Nigeria with registration code 1924392.
+          </p>
+
+          <h4>Bube app use</h4>
+
+          <p>
+            The Bube app links individuals who need rides with drivers. Bube doesn't
+            provide transport. Contract-based drivers carry people (with you). Drivers
+            provide transportation services alone or via a corporation. Professional and
+            cost-effective. Bube isn't responsible for enforcing the passenger-driver
+            agreement. Passengers and drivers may quarrel about consumer rights, legal
+            duties, or transportation laws. Bube's app lists drivers and their services.
+            Receipts are emailed to the passenger's email address.
+          </p>
+          <p>
+            1.2 As a passenger, you arrange transportation using the Bube app. Depending
+            on your trip's location, you may pay the driver with cash or with Bube
+            in-App Payment. Bube pays Separate agreements cover business travel. Prices
+            include legal taxes. Booking costs, municipal tolls, airport surcharges, and
+            split payment processing fees may also be charged. You may tip the driver
+            directly or using Bube in-App Payment. We may limit a Tip's value.
+          </p>
+          <p>
+            1.3 A passenger's phone number is connected to their Bube account when they
+            install the app. Bube must be notified within 7 days if you no longer use
+            your mobile number. Your cell operator may give out your old number if you
+            don't inform us. If they utilize Bube, they can access your data.
+          </p>
+
+          <h4>Promo coupons</h4>
+
+          <p>
+            2.1 Bube may send separate promo coupons. A promotional code may be used to
+            pay for a ride or gain additional features or advantages from the service or
+            a Third Party. Each promotional code has restrictions. Once you add a
+            discount code, the app will display its expiry date.
+          </p>
+          <p>
+            2.2 If your journey costs more than your credit, the difference will be
+            instantly deducted. A coupon code is good for one ride. It's gone since it
+            can't be utilized again. One voucher per trip.
+          </p>
+          <p>
+            Bube may revoke any discount coupon at any moment. This includes if Bube
+            believes the codes are being used unlawfully or fraudulently, were handed
+            out by mistake, or have expired.
+          </p>
+
+          <h5>3. Bube in-app payment</h5>
+
+          <p>
+            3.1 Depending on the travel location, you may pay for Bube services using a
+            credit card, cell carrier billing, or other means (like Bube Business). Bube
+            works as a business agent for transportation firms by allowing in-app
+            payments. Every motorist has granted Bube authorization to serve as their
+            business agent. This includes letting Bube take passenger payments for
+            drivers. When you send money to Bubes' bank account, you've satisfied your
+            service provider's commitment. As a passenger, you must pay and have
+            adequate money.
+          </p>
+          <p>
+            3.2 You may tip using Bube In-app Payment. The Tip may be purchased with
+            Bube In-App Payment. Bube won't take a percentage of the Tip as a brokering
+            charge; the driver will get the entire amount, less taxes. Bube may withhold
+            a tip if it was paid fraudulently, unlawfully, for a cause other than a tip
+            for the service, or in a manner that violates its terms and conditions.
+          </p>
+          <p>
+            3.3 Bube transfers the driver the money when you pay using in-app payment.
+            Bube may ask for extra payment details.
+          </p>
+          <p>
+            3.4 Bube is not liable for third-party payment expenses while using Bube
+            in-app Payment (e.g mobile operators, bank fees). These service providers
+            may impose additional fees for Bube in-App Payments. Bube doesn't cover
+            these payments. Your payment method may be subject to third-party
+            restrictions and limitations. Before using your payment method, read these
+            terms.
+          </p>
+          <p>
+            3.5 Bube will ensure in-app payment works and address bugs. We resolve Bube
+            in-App Payment disputes. Email info@Bube.app for payment support. You'll
+            receive a response to an email or Bube app query within one business day.
+            Bube will handle complaints and applications within two business days.
+          </p>
+          <p>
+            3.6 Upfront fare. You may be able to agree on a set Fare for a Driver's
+            service (i.e Upfront Fare). The Bube app shows the upfront fee before a
+            journey. Upfront Fare won't be charged if you change your location, if the
+            journey takes longer than planned due to traffic or other factors, or if
+            anything else unexpected occurs (e.g a route is used where tolls apply).
+          </p>
+
+          <h5>4. Transportation services acquisition and disposal</h5>
+
+          <p>
+            4.1 You've requested a transportation service if the driver agrees to
+            perform it.
+          </p>
+          <p>
+            4.2 Once a driver agrees to transport you, you'll arrange a separate
+            contract with them. You and the driver will set travel terms. Bube doesn't
+            give rides, and neither does the driver.
+          </p>
+          <p>
+            4.3 You terminate a requested transportation service when the driver returns
+            and you reject, cancel, or deny it. After a certain period, you must pay a
+            cancellation charge if you cancel a transport request.
+          </p>
+          <p>
+            4.4 We may temporarily disable your account if you cancel a transport
+            request more than once in 24 hours. If you get several warnings, we may
+            suspend your account (e.g 6 months). After that, you might ask Bube to
+            restore your account.
+          </p>
+          <p>
+            4.5 If the driver notifies the passenger that the vehicle has arrived, but
+            the passenger or individuals who hired the trip don't come within a given
+            time, the request will be canceled. Bube isn't liable if a driver cancels
+            your request.
+          </p>
+          <p>
+            4.6 Once the driver comes and notifies you, the Bube app may start charging
+            you for waiting time.
+          </p>
+          <p>
+            4.7 If you use the Bube app to seek a ride and damage the driver's car or
+            interior (by leaving scratches or stains on it or making it smell terrible),
+            the driver may charge you a 10,000 Naira fine and ask for restitution for
+            any damages that are larger than the fine. Bube may pay the fee or repair
+            the damage if you don't.
+          </p>
+
+          <h4>Bube app license</h4>
+
+          <p>
+            5.1 We grant you a royalty-free, revocable, non-exclusive license to access
+            and use the Bube app in accordance with these General Rules and Conditions,
+            the Privacy Notice, and the app store terms. You can't let someone else use
+            the Bube app. If you can't utilize the Bube app, you lose the non-exclusive
+            license.
+          </p>
+
+          <h5>Liability</h5>
+
+          <p>
+            6.1 Since the Bube app is an information society service (a mechanism for
+            passengers and drivers to communicate), we can't guarantee or be liable for
+            transportation quality or difficulties. Bube does not guarantee that drivers
+            will always have transportation opportunities because of how they utilize
+            the program.
+          </p>
+          <p>
+            6.2 Bube doesn't provide or organize passenger transportation. It's also not
+            a ride-matching service. Bube plans transportation services.
+          </p>
+          <p>
+            Bube app orders are non-refundable. If you seek for a refund, you're still
+            bound by the original agreement.
+          </p>
+          <p>
+            6.4 The Bube app is released "as is" Bube does not promise, assure, or
+            ensure uninterrupted app usage. We'll resolve software issues as quickly as
+            feasible. We can't guarantee that the app will always operate due to
+            periodic technical issues. An emergency might disrupt service.
+          </p>
+          <p>
+            6.5 Bube, its agents, directors, and employees are not liable for any loss
+            or damage you suffer due of the Bube app or a Bube-booked trip. Included
+            are:
+          </p>
+          <p>
+            6.5.1. direct or indirect property damage or money loss; 6.5.2. loss of
+            profit; 6.5.3. loss of business, contracts, contacts, goodwill, reputation,
+            and other business disruption losses; 6.5.4. loss of data or erroneous data;
+            and 6.5.5. any other damage or loss.
+          </p>
+          <p>
+            6.6 Bube is only accountable for 500 euros if it breaches the contract. Bube
+            won't pay until they intentionally breached the deal. Bube isn't liable for
+            the driver's actions or damages to passengers.
+          </p>
+          <p>
+            6.7 You agree to keep Bube, their associated businesses, agents, employees,
+            and directors free from any claims or losses caused by your use of the Bube
+            app (including the journeys you obtain through your use of the Bube app).
+          </p>
+          <p>
+            6.8 Bube may immediately ban you from using the Bube app if you breach these
+            terms or if we consider it's essential to safeguard Bube's reputation or
+            drivers' safety.
+          </p>
+
+          <h5>7. Use Bube app</h5>
+
+          <p>
+            7.1 Since Bube is neither a transport service provider nor a broker, any
+            quality or defect issues will be addressed by the transport service provider
+            or the responsible public body.
+          </p>
+          <p>7.2 In Bube, we want input. This helps us enhance the drivers' service.</p>
+          <p>
+            7.3 Use the Bube app honestly and respect its drivers. Bube retains the
+            right to terminate your account if you violate these General Terms and
+            Conditions or do anything terrible, such as not paying for the service,
+            committing fraud, or being disrespectful to the drivers. If any of these
+            things happen, your Bube app account might be closed.
+          </p>
+          <p>
+            7.4 Bube will do all it can to ensure only honest, respectful drivers use
+            the program. We can't guarantee that every Bube app-found transportation
+            provider meets these standards. If you're unhappy with a transportation
+            service, contact the firm, a regulatory body, or our customer service.
+          </p>
+
+          <h5>8. Terms and Conditions Changes</h5>
+
+          <p>
+            8.1 You'll be notified via e-mail or the Bube app of any substantial changes
+            to the Terms and Conditions. Using the Bube app means you agree to the
+            modifications.
+          </p>
+
+          <h5>9. Rules</h5>
+
+          <p>
+            The General Terms and Conditions are governed by Nigerian legislation.
+            Disputes over the General Terms or Agreement shall be resolved by the
+            Federal High Court in Nigeria. If any section of the General Terms is
+            unenforceable, the parties will replace it with a component that is similar
+            to what the unenforceable part was attempting to achieve and how it would
+            effect the company.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PassengersTerms;
